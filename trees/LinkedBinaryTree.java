d231ab260a550a512a74fab48c0be762f4026ae3
Abbas Mohamed Abbas <m3zaelray2@gmail.com>
2022-05-28 16:45:01 +0200

    Code

---
+++ trees/LinkedBinaryTree.java
@@ -0,0 +1,226 @@
+package trees;
+
+import java.util.Iterator;
+
+public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
+	
+	
+
+	protected static class Node<E> implements Position<E> {
+		private E element;
+		private Node<E> parent;
+		private Node<E> left;
+		private Node<E> right;
+		
+
+		public Node(E e, Node<E> above, Node<E> leftChild, Node<E> rightChild) {
+			element = e;
+			parent = above;
+			left = leftChild;
+			right = rightChild;
+		}
+
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getParent() {
+			return parent;
+		}
+
+		public Node<E> getLeft() {
+			return left;
+		}
+
+		public Node<E> getRight() {
+			return right;
+		}
+
+		public void setElement(E e) {
+			element = e;
+		}
+
+		public void setParent(Node<E> parentNode) {
+			parent = parentNode;
+		}
+
+		public void setLeft(Node<E> leftChild) {
+			left = leftChild;
+		}
+
+		public void setRight(Node<E> rightChild) {
+			right = rightChild;
+		}
+	}
+	public static void main(String[] args) {
+		LinkedBinaryTree<Integer> abbas_tree = new LinkedBinaryTree<Integer>() ;
+		Node <Integer> root=(Node <Integer>) abbas_tree.addRoot(10);
+		abbas_tree.addLeft(root, 9);
+		abbas_tree.addRight(root, 11);
+		abbas_tree.addLeft(root.getLeft(),5);
+		abbas_tree.addRight(root.getLeft(),3);
+		abbas_tree.addLeft(root.getRight(),7);
+		abbas_tree.addRight(root.getRight(),8);	
+		System.out.println("\n the postorder traversal:");
+		abbas_tree.PostOrder(root);
+		System.out.println("\n the preorder traversal:");
+		abbas_tree.PreOrder(root);
+
+
+	}
+     public void PostOrder(Position<E> p) {
+    	 Node<E>node=(Node<E>)p;
+    	 if(p==null)
+    		 return;
+    	 PostOrder(node.getLeft());
+    	 PostOrder(node.getRight());
+    	 System.out.print(node.getElement()+"    "); 	 
+     }
+     public void PreOrder(Position<E> p) {
+    	 Node<E>node=(Node<E>)p;
+    	 if(p==null)
+    		 return;
+    	 System.out.print(node.getElement()+"    ");
+    	 PreOrder(node.getLeft());
+    	 PreOrder(node.getRight());
+     }
+     
+	/** Factory function to create a new node storing element e. */
+	protected Node<E> createNode(E e, Node<E> parent, Node<E> left, Node<E> right) {
+		return new Node<E>(e, parent, left, right);
+	}
+
+	protected Node<E> root = null;
+	private int size = 0;
+
+	public LinkedBinaryTree() {
+	}
+
+	/** Validates the position and returns it as a node. */
+	protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
+		if (!(p instanceof Node))
+			throw new IllegalArgumentException("Not valid position type");
+		Node<E> node = (Node<E>) p;
+		if (node.getParent() == node)
+			// our convention for defunct node
+			throw new IllegalArgumentException("p is no longer in the tree");
+		return node;
+	}
+
+	// accessor methods (not already implemented in AbstractBinaryTree)
+	/** Returns the number of nodes in the tree. */
+	public int size() {
+		return size;
+	}
+
+	/** Returns the root Position of the tree (or null if tree is empty). */
+	public Position<E> root() {
+		return root;
+	}
+
+	/** Returns the Position of p's parent (or null if p is root). */
+	public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return node.getParent();
+	}
+
+	/** Returns the Position of p's left child (or null if no child exists). */
+	public Position<E> left(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return node.getLeft();
+	}
+
+	/** Returns the Position of p's right child (or null if no child exists). */
+	public Position<E> right(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return node.getRight();
+	}
+
+	// update methods supported by this class
+	/**
+	 * Places element e at the root of an empty tree and returns its new Position.
+	 */
+	public Position<E> addRoot(E e) throws IllegalStateException {
+		if (!isEmpty())
+			throw new IllegalStateException("Tree is not empty");
+		root = createNode(e, null, null, null);
+		size = 1;
+		return root;
+	}
+
+	/**
+	 * Creates a new left child of Position p storing element e; returns its
+	 * Position.
+	 */
+	public Position<E> addLeft(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> parent = validate(p);
+		if (parent.getLeft() != null)
+			throw new IllegalArgumentException("p already has a left child");
+		Node<E> child = createNode(e, parent, null, null);
+		parent.setLeft(child);
+		size++;
+		return child;
+	}
+
+	/**
+	 * Creates a new right child of Position p storing element e; returns its
+	 * Position.
+	 */
+	public Position<E> addRight(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> parent = validate(p);
+		if (parent.getRight() != null)
+			throw new IllegalArgumentException("p already has a right child");
+		Node<E> child = createNode(e, parent, null, null);
+		parent.setRight(child);
+		size++;
+		return child;
+	}
+
+	/**
+	 * Replaces the element at Position p with e and returns the replaced element.
+	 */
+	public E set(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		E temp = node.getElement();
+		node.setElement(e);
+		return temp;
+	}
+
+	/** Removes the node at Position p and replaces it with its child, if any. */
+	public E remove(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		if (numChildren(p) == 2)
+			throw new IllegalArgumentException("p has two children");
+		Node<E> child = (node.getLeft() != null ? node.getLeft() : node.getRight());
+		if (child != null)
+			child.setParent(node.getParent()); // child’s grandparent becomes its parent
+		if (node == root)
+			root = child;
+		else {
+			Node<E> parent = node.getParent();
+			if (node == parent.getLeft())
+				parent.setLeft(child);
+			else
+				parent.setRight(child);
+		}
+		size--;
+		E temp = node.getElement();
+		node.setElement(null);
+		node.setLeft(null);
+		node.setRight(null);
+		node.setParent(node);
+		return temp;
+    }
+    @Override
+	public Iterator<E> iterator() {
+		return null;
+	}
+
+	@Override
+	public Iterable<Position<E>> positions() {
+		return null;
+	}
+}
+
+
+
